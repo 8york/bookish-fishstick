f1d6ef80a8a7a1f3d2884c5c7e72201115d3d2a9
Akiko Yamasaki <paonpaon@gmail.com>
2023-04-05 09:44:36 +1000

    Wed Commit

--- js/main.js
+++ js/main.js
@@ -1,56 +1,69 @@
-$(document).ready(function(){
-    
-    const player = ["X", "O"];
-    let currentPlayer = "X";
-    currentPosition = {
-        X:[], O:[]
-    }
-    let gameOn = false;
-   $('.cell').on('click', function(){
-    
+$(document).ready(function () {
+  let currentPlayer = "X";
+  let currentPosition = {
+    X: [],
+    O: [],
+  };
+
+  $(".cell").on("click", function () {
     $(this).text(currentPlayer);
-    // console.log(this);
-    currentPosition[currentPlayer].push(parseInt($(this).attr('data-index')));
-    // console.log($(this).attr('data-index'));
-    // console.log(currentPosition);
-    if(currentPlayer === 'X'){
-        currentPlayer = "O";
-        $('h2').html(`${currentPlayer}'s turn`);
+    currentPosition[currentPlayer].push(parseInt($(this).attr("data-index")));
+    if (currentPlayer === "X") {
+      currentPlayer = "O";
+      $("#message").html(`${currentPlayer}'s turn`);
+    } else if (currentPlayer === "O") {
+      currentPlayer = "X";
+      $("#message").html(`${currentPlayer}'s turn`);
+    } else if ($(".cell").length < 0) {
+      console.log($(".cell"));
+      $("#message").html(`It's a draw`).addClass("winner");
     }
-    else if(currentPlayer === "O"){
-        currentPlayer = "X";
-        $('h2').html(`${currentPlayer}'s turn`);
+    winnerIs();
+  });
+
+  const winningGrid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [3, 5, 7],
+    [1, 5, 9],
+  ];
+
+  const winnerIs = function () {
+    for (combination of winningGrid) {
+      const [a, b, c] = combination;
+      if (
+        currentPosition.X.includes(a) &&
+        currentPosition.X.includes(b) &&
+        currentPosition.X.includes(c)
+      ) {
+        $("#message").html(`X wins`).addClass("winner");
+        //not working
+        $(currentPosition.X).css("background-color", "violet");
+      } else if (
+        currentPosition.O.includes(a) &&
+        currentPosition.O.includes(b) &&
+        currentPosition.O.includes(c)
+      ) {
+        $("#message").html(`O wins`).addClass("winner");
+      }
     }
-        winnerIs();
-   });
-    
+  };
 
-//winning function
-//make an array of winning index
-//loop through and create another loop to go through each number of array item
+  const restart = function () {
+    $(".cell").text("");
+    $("#message").text(`It's X's turn`).removeClass("winner");
+    currentPlayer = "X";
+    currentPosition = {
+      X: [],
+      O: [],
+    };
+  };
 
-const winningGrid = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [3, 5, 7], [1, 5, 9]];
+  $("#btn").on("click", restart);
 
-const winnerIs = function(){
-    console.log('working');
-     for (let i = 0; i < winningGrid.length; i++){
-        let winner = true;
-         for (let j = 0; j < winningGrid[i].length; j++){
-        // console.log(winningGrid[i]);
-        console.log(currentPosition.X);
-        console.log(winningGrid[i]);
-        if(currentPosition.X === winningGrid[i]){
-        //    console.log($('h2').html(`the ${currentPlayer} wins`));
-        console.log("you have won");
-        }else{
-            console.log('notworking');
-        }  
-     }
-    }
-     
-}
-
-
-// const restart = function ()
-
-})
+  const winnningGridBgColor = function () {};
+});
